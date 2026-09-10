bae95169a56ec75013084b83a9f2612561e4bcdd
Aditya Sharma <adityas@technoidentity.com>
2026-09-10 21:36:45 +0530

    Add two comments.

---
+++ rustBasics/week1/firstProgram.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hey, here I am able to run it alone without cargo!");
+    println!("Hey, this is my second line!");
+    // this works now !!!
+    // This also works now
+}
