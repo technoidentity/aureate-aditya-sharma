d3bdfa92bd45bdbc84e02315d7e036405166c0af
Aditya Sharma <adityas@technoidentity.com>
2026-09-12 20:23:13 +0530

    Add fully functional guess the number code.

--- rustBasics/week1/guessTheNumber.rs
+++ rustBasics/week1/guessTheNumber.rs
@@ -1,12 +1,31 @@
+use colored::*;
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("Guess the number!");
-    println!("Please in put your guess: ");
-    let secretNumber = rand::thread_rng().gen_range(1, 101);
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-    println!("You guessed: {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101); // High is exclusive.
+
+    loop {
+        println!("Please in put your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+        println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }; // The concept of shadowing.
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You guessed it correctly! You Win!".green());
+                break;
+            }
+        };
+    }
 }
